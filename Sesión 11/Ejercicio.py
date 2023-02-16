854e3689a8b6b08389868168d447534bb2427e6c
Cristofer Azaña <cristoferprograma@gmail.com>
2023-02-16 17:47:16 -0500

    database

--- Sesión 11/Ejercicio.py
+++ Sesión 11/Ejercicio.py
@@ -9,12 +9,29 @@ cursor = conn.cursor()
 cursor.execute('CREATE TABLE Alumnos (id INTEGER, nombre TEXT, apellido TEXT)')
 
 # Insertar Datos en la tabla
-lista = [ (12, 'Cristofer Azaña', 'Azaña'), (2, 'Fernando', 'Cherre'), (29, 'Kevin', 'Salinas')]
+lista = [ (12, 'Cristofer', 'Azaña'), (2, 'Fernando', 'Cherre'), (29, 'Kevin', 'Salinas')]
 cursor.executemany('INSERT INTO Alumnos (id, nombre, apellido) VALUES (?,?,?)', lista)
+
+# Función para buscar un alumno por nombre y mostrar los datos por consola
+def buscar_alumno(nombre):
+    consulta = 'SELECT id, nombre, apellido FROM Alumnos WHERE nombre=?'
+    cursor.execute(consulta, (nombre,))
+    resultado = cursor.fetchone()
+    if resultado:
+        print('ID:', resultado[0])
+        print('Nombre:', resultado[1])
+        print('Apellido:', resultado[2])
+    else:
+        print('El alumno no se encontró.')
+
+# Ejemplo de uso de la función buscar_alumno
+buscar_alumno('Fernando')
+
+
 
 # Guardar cambios y cerrar
 conn.commit()                   
-conn.close()  
-# Oe PUTMADRE odio Git crjaaaaa
+conn.close()
+
  
 
