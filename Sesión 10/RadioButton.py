ba60302809bd79704207cae14a7127705a1b0acc
Cristofer Azaña <cristoferprograma@gmail.com>
2023-02-18 17:22:30 -0500

    Finish Course

--- Sesión 10/RadioButton.py
+++ Sesión 10/RadioButton.py
@@ -5,6 +5,7 @@ root = tk.Tk()
 root.title("Botones de radio")
 root.geometry("300x200")
 
+
 # Función para manejar el evento de los botones de radio
 def handle_radio_event():
     label.config(text="Opción seleccionada: " + str(var.get()))
@@ -19,6 +20,7 @@ radio_1 = tk.Radiobutton(root, text="Opción 1", variable=var, value=1, command=
 radio_2 = tk.Radiobutton(root, text="Opción 2", variable=var, value=2, command=handle_radio_event)
 radio_3 = tk.Radiobutton(root, text="Opción 3", variable=var, value=3, command=handle_radio_event)
 
+
 # Empaquetamiento de los botones de radio
 radio_1.pack()
 radio_2.pack()
@@ -31,3 +33,4 @@ label.pack()
 
 # Inicio del bucle de eventos
 root.mainloop() 
+
